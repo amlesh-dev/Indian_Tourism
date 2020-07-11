81e7df2703af158667db1c3037537a901acc8276
Amlesh kumar <53970086+amlesh-dev@users.noreply.github.com>
2020-07-11 22:34:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  
+
+	
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Indian Turorism</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li> 
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/1111.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Akshardham</h3>
+        <p>Swaminarayan Akshardham (New Delhi) is a Hindu temple, and a spiritual-cultural campus in New Delhi, India</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/tajmahal.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Taj Mahal</h3>
+        <p>Symbol of Love</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/redford.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Red Ford </h3>
+        <p>The Red Fort is a historic fort in the city of Delhi in India that served as the main residence of the Mughal Emperors.</p>
+      </div>   
+    </div>
+  </div>
+
+
+
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+	<div class="py-5">
+		<h3 class="text-center">About Us</h3>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+				<div class="col-lg-6 col-md-6 col-12">
+				<img src="image/indiagate.jpg" class="img-fluid">
+				</div>
+				<div class="col-lg-6 col-md-6 col-12">
+				<h2>Incredible India</h2>
+				<p class="py-3">
+				India never invaded any country in her last 100000 years of history.
+When many cultures were only nomadic forest dwellers over 5000 years ago, Indians established Harappan culture in Sindhu Valley (Indus Valley Civilization)
+The name 'India' is derived from the River Indus, the valleys around which were the home of the early settlers. The Aryan worshippers referred to the river Indus as the Sindhu.
+The Persian invaders converted it into Hindu. The name 'Hindustan' combines Sindhu and Hindu and thus refers to the land of the Hindus.
+Chess was invented in India.
+Algebra, Trigonometry and Calculus are studies, which originated in India.
+The 'Place Value System' and the 'Decimal System' were developed in India in 100 B.C.
+The World's First Granite Temple is the Brihadeswara Temple at Tanjavur, Tamil Nadu. The shikhara of the temple is made from a single 80-tonne piece of granite. This magnificent temple was built in just five years, (between 1004 AD and 1009 AD) during the reign of Rajaraja Chola.
+India is the largest democracy in the world, the 7th largest Country in the world, and one of the most ancient civilizations.
+The game of Snakes & Ladders was created by the 13th century poet saint Gyandev. It was originally called 'Mokshapat'. The ladders in the game represented virtues and the snakes indicated vices. The game was played with cowrie shells and dices. In time, the game underwent several modifications, but its meaning remained the same, i.e. good deeds take people to heaven and evil to a cycle of re-births.
+The world's highest cricket ground is in Chail, Himachal Pradesh. Built in 1893 after leveling a hilltop, this cricket pitch is 2444 meters above sea level.
+India has the largest number of Post Offices in the world.
+The largest employer in India is the Indian Railways, employing over a million people.
+The world's first university was established in Takshila in 700 BC. More than 10,500 students from all over the world studied more than 60 subjects. The University of Nalanda built in the 4th century was one of the greatest achievements of ancient India in the field of education.
+Ayurveda is the earliest school of medicine known to mankind. The Father of Medicine, Charaka, consolidated Ayurveda 2500 years ago.
+India was one of the richest countries till the time of British rule in the early 17th Century. Christopher Columbus, attracted by India's wealth, had come looking for a sea route to India when he discovered America by mistake.
+The Art of Navigation & Navigating was born in the river Sindh over 6000 years ago. The very word Navigation is derived from the Sanskrit word 'NAVGATIH'. The word navy is also derived from the Sanskrit word 'Nou'.
+Bhaskaracharya rightly calculated the time taken by the earth to orbit the Sun hundreds of years before the astronomer Smart. According to his calculation, the time taken by the Earth to orbit the Sun was 365.258756484 days.
+The value of "pi" was first calculated by the Indian Mathematician Budhayana, and he explained the concept of what is known as the Pythagorean Theorem. He discovered this in the 6th century, long before the European mathematicians.
+				<a href="about.php">More....</a>
+				</div>
+		</div>
+	</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h3 class="text-center">Our Services </h3>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" style="width:400px">
+                <img class="card-img-top" src="image/5.jpg" alt="Card image">
+  				<div class="card-body">
+  				  <h4 class="card-title">Gautama Buddha</h4>
+   				 <p class="card-text">The Buddha was a philosopher, mendicant, meditator, spiritual teacher, and religious leader who lived in Ancient India.</p>
+   				 <a href="https://en.wikipedia.org/wiki/Gautama_Buddha" class="btn btn-primary">Read More</a>
+ 				 </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" style="width:400px">
+                <img class="card-img-top" src="image/he.jpg" alt="Card image">
+  				<div class="card-body">
+  				  <h4 class="card-title">God Bajrangbali</h4>
+   				 <p class="card-text">Hanuman is a Hindu god and divine vanara companion of the god Rama. Hanuman is one of the central characters of the Hindu epic Ramayana.</p>
+   				 <a href="https://en.wikipedia.org/wiki/Hanuman" class="btn btn-primary">Read More</a>
+ 				 </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card" style="width:400px">
+                <img class="card-img-top" src="image/s.jpg" alt="Card image">
+  				<div class="card-body">
+  				  <h4 class="card-title">Lord Rama</h4>
+   				 <p class="card-text">Rama also known as Ramachandra, is a major deity of Hinduism. He is the seventh avatar of the god Vishnu, one of his most popular incarnations along with Krishna, Parshurama, and Gautama Buddha.</p>
+   				 <a href="https://en.wikipedia.org/wiki/Rama" class="btn btn-primary">Read More</a>
+ 				 </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h3 class="text-center">Our Gallery </h3>
+	</div>
+	<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/8.jpg" class="img-fluid pb-4" >
+			<a href="https://en.wikipedia.org/wiki/Ayodhya"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/7.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Murdeshwar"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/4.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Trimbakeshwar_Shiva_Temple"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/i.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Mahavir_Mandir"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/si.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Kalighat_Kali_Temple"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/su.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Siddhivinayak_Temple,_Mumbai"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/ro.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Ranganathaswamy_Temple,_Srirangam"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/gi.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Padmanabhaswamy_Temple"class="btn btn-primary">Read More</a>
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+			<img src="image/ab.jpg" class="img-fluid pb-4">
+			<a href="https://en.wikipedia.org/wiki/Shiv_Mandir,_Ambarnath"class="btn btn-primary">Read More</a>
+		</div>
+	</div>		
+	</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h3 class="text-center">FeedBack Form </h3>
+	</div>
+	<div class="w-50 m-auto">
+		<form action="userinfo.php" method="post">
+			<div class="form-group">
+				<label>UserName</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email Id</label>
+				<input type="text" name="email" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Mobile</label>
+				<input type="text" name="mobile" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Comment</label>
+				<textarea class="form-control" name="comment"></textarea>
+			</div>
+			<button type="submit" class="btn btn-success">Submit</button>
+		</form>
+	</div>
+</section>
+
+<footer>
+	<p class="p-3 bg-dark text-white text-center">@amleshdevtourism</p>
+</footer>
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
