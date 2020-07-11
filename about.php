3e1fdff5f6ec9547d9c397dcf82f07ce88329d1b
Amlesh kumar <53970086+amlesh-dev@users.noreply.github.com>
2020-07-11 22:35:24 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/style3.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  
+
+	
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Indian Turorism</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>Incredible India</h1>
+  <p>Incredible People</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h3 class="text-center">About Us</h3>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+        <img src="image/3.jpg" class="img-fluid">
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+        <h2>Incredible India</h2>
+        <p class="py-3">
+          <p> >India never invaded any country in her last 100000 years of history.</p>
+<p> >When many cultures were only nomadic forest dwellers over 5000 years ago, Indians established Harappan culture in Sindhu Valley (Indus Valley Civilization)</p>
+<p>The name 'India' is derived from the River Indus, the valleys around which were the home of the early settlers. The Aryan worshippers referred to the river Indus as the Sindhu.</p>
+<p> >The Persian invaders converted it into Hindu. The name 'Hindustan' combines Sindhu and Hindu and thus refers to the land of the Hindus.</p>
+<p> >Chess was invented in India.</p>
+<p>>Algebra, Trigonometry and Calculus are studies, which originated in India.</p>
+<p>>The 'Place Value System' and the 'Decimal System' were developed in India in 100 B.C.</p>
+<p>>The World's First Granite Temple is the Brihadeswara Temple at Tanjavur, Tamil Nadu. The shikhara of the temple is made from a single 80-tonne piece of granite. This magnificent temple was built in just five years, (between 1004 AD and 1009 AD) during the reign of Rajaraja Chola.</p>
+<p>>India is the largest democracy in the world, the 7th largest Country in the world, and one of the most ancient civilizations.</p>
+<p>>The game of Snakes & Ladders was created by the 13th century poet saint Gyandev. It was originally called 'Mokshapat'. The ladders in the game represented virtues and the snakes indicated vices. The game was played with cowrie shells and dices. In time, the game underwent several modifications, but its meaning remained the same, i.e. good deeds take people to heaven and evil to a cycle of re-births.</p>
+<p>>The world's highest cricket ground is in Chail, Himachal Pradesh. Built in 1893 after leveling a hilltop, this cricket pitch is 2444 meters above sea level.</p>
+<p>>India has the largest number of Post Offices in the world.</p>
+<p>>The largest employer in India is the Indian Railways, employing over a million people.</p>
+<p>>The world's first university was established in Takshila in 700 BC. More than 10,500 students from all over the world studied more than 60 subjects. The University of Nalanda built in the 4th century was one of the greatest achievements of ancient India in the field of education.</p>
+<p>>Ayurveda is the earliest school of medicine known to mankind. The Father of Medicine, Charaka, consolidated Ayurveda 2500 years ago.</p>
+<p>>India was one of the richest countries till the time of British rule in the early 17th Century. Christopher Columbus, attracted by India's wealth, had come looking for a sea route to India when he discovered America by mistake.</p>
+<p>>The Art of Navigation & Navigating was born in the river Sindh over 6000 years ago. The very word Navigation is derived from the Sanskrit word 'NAVGATIH'. The word navy is also derived from the Sanskrit word 'Nou'.</p>
+<p>>Bhaskaracharya rightly calculated the time taken by the earth to orbit the Sun hundreds of years before the astronomer Smart. According to his calculation, the time taken by the Earth to orbit the Sun was 365.258756484 days.</p>
+<p>>The value of "pi" was first calculated by the Indian Mathematician Budhayana, and he explained the concept of what is known as the Pythagorean Theorem. He discovered this in the 6th century, long before the European mathematicians.</p>
+<p>>Algebra, Trigonometry and Calculus also originated in India.Quadratic Equations were used by Sridharacharya in the 11th century. The largest numbers the Greeks and the Romans used were 106 whereas Hindus used numbers as big as 10*53 (i.e. 10 to the power of 53) with specific names as early as 5000 B.C.during the Vedic period.Even today, the largest used number is Terra: 10*12(10 to the power of 12).</p>
+<p>>Until 1896, India was the only source of diamonds in the world(Source: Gemological Institute of America).</p>
+<p>>The Baily Bridge is the highest bridge in the world. It is located in the Ladakh valley between the Dras and Suru rivers in the Himalayan mountains. It was built by the Indian Army in August 1982.</p>
+<p>>Sushruta is regarded as the Father of Surgery. Over2600 years ago Sushrata & his team conducted complicated surgeries like cataract, artificial limbs, cesareans, fractures, urinary stones, plastic surgery and brain surgeries.</p>
+<p>>Usage of anaesthesia was well known in ancient Indian medicine. Detailed knowledge of anatomy, embryology, digestion, metabolism,physiology, etiology, genetics and immunity is also found in many ancient Indian texts.</p>
+<p>>India exports software to 90 countries.</p>
+<p>>The four religions born in India - Hinduism, Buddhism, Jainism, and Sikhism, are followed by 25% of the world's population.</p>
+<p>>Jainism and Buddhism were founded in India in 600 B.C. and 500 B.C. respectively.</p>
+<p>>Islam is India's and the world's second largest religion.</p>
+<p>>There are 300,000 active mosques in India, more than in any other country, including the Muslim world.</p>
+<p>>The oldest European church and synagogue in India are in the city of Cochin. They were built in 1503 and 1568 respectively.</p>
+<p>>Jews and Christians have lived continuously in India since 200 B.C. and 52 A.D. respectively.</p>
+<p>>The largest religious building in the world is Angkor Wat, a Hindu Temple in Cambodia built at the end of the 11th century.</p>
+<p>>The Vishnu Temple in the city of Tirupathi built in the 10th century, is the world's largest religious pilgrimage destination. Larger than either Rome or Mecca, an average of 30,000 visitors donate $6 million (US) to the temple everyday.</p>
+<p>>Sikhism originated in the Holy city of Amritsar in Punjab. Famous for housing the Golden Temple, the city was founded in 1577.</p>
+<p>>Varanasi, also known as Benaras, was called "the Ancient City" when Lord Buddha visited it in 500 B.C., and is the oldest, continuously inhabited city in the world today.</p>
+<p>>India provides safety for more than 300,000 refugees originally from Sri Lanka, Tibet, Bhutan, Afghanistan and Bangladesh, who escaped to flee religious and political persecution.</p>
+<p>>His Holiness, the Dalai Lama, the exiled spiritual leader of Tibetan Buddhists, runs his government in exile from Dharmashala in northern India.</p>
+<p>>Martial Arts were first created in India, and later spread to Asia by Buddhist missionaries.</p>
+<p>>Yoga has its origins in India and has existed for over 5,000 years.</p>
+        </p>
+        </div>
+    </div>
+  </div>
+</section>
+
+
+<div class="about-section">
+  <h1>About Us Page</h1>
+  <p>Some text about who we are and what we do.</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Amlesh Kumar</h2>
+        <p class="title">Student</p>
+        <p>We are NITKain</p>
+        <p>amleshkumar11081@gmail.com</p>
+        <a href="contact.php"class="btn btn-success">Contact</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Sannat Doluai</h2>
+        <p class="title">Student</p>
+        <p>we are NITKian</p>
+        <p>sanatdolui111@gmail.com</p>
+        <a href="contact.php"class="btn btn-success">Contact</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@amleshdevtourism</p>
+</footer>
+
+
+
+</body>
+</html>
